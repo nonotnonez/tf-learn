a35e88c234ce03864f4c8e4e6da48f69ee05d347
trantuanfoto <trantuanfoto@gmail.com>
2024-04-16 16:42:13 +0700

    prefix

--- main.tf
+++ main.tf
@@ -1,32 +1,29 @@
-variable "cidr_blocks" {
-    description = "cidr blocks and name tags for vpc and subnets"
-    type = list(object({
-        cidr_block = string
-        name = string
-    }))
+variable vpc_cidr_block {}
+variable subnet_cidr_block {}
+variable avail_zone {}
+variable env_prefix {}
+
+resource "aws_vpc" "myapp-vpc" {
+  cidr_block = var.vpc_cidr_block
+  tags = {
+      Name = "${var.env_prefix}-vpc"
+  }
 }
 
-resource "aws_vpc" "development-vpc" {
-    cidr_block = var.cidr_blocks[0].cidr_block
-    tags = {
-        Name = var.cidr_blocks[0].name
-    }
-}
-
-resource "aws_subnet" "development-subnet-1" {
-    vpc_id = aws_vpc.development-vpc.id
-    cidr_block = var.cidr_blocks[1].cidr_block
-    availability_zone = "ap-southeast-1b"
-    tags = {
-        Name = var.cidr_blocks[1].name
-    }
+resource "aws_subnet" "myapp-subnet-1" {
+  vpc_id = aws_vpc.myapp-vpc.id
+  cidr_block = var.subnet_cidr_block
+  availability_zone = var.avail_zone
+  tags = {
+      Name = "${var.env_prefix}-subnet-1"
+  }
 }
 
 output "dev-vpc-id" {
-    value = aws_vpc.development-vpc.id
+    value = aws_vpc.myapp-vpc.id
 }
 
 output "dev-subnet-id" {
-    value = aws_subnet.dev-subnet-1.id
+    value = aws_subnet.myapp-subnet-1.id
 }
 
